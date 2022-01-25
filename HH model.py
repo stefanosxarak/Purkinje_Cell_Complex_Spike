7767992636bf7e4ba88328943b3d26bcee819402
stefanosxarak <stefanosxara@gmail.com>
2022-01-25 19:55:18 +0000

    implemented derivative function

--- HH model.py
+++ HH model.py
@@ -6,14 +6,7 @@ from scipy.integrate import odeint
 
 # Hodgkin - Huxley model
 
-# V_m is the membrane potential
-# g_k and g_na are the potassium and sodium conductances per unit area
-# g_l and V_l are the leak conductance per unit area and leak reversal potential
-I_na = g_na * (V_m - V)
-I_k = g_k * (V_m - V)
-I_l = g_l * (V_m - V)
-
-# α and β are the forward and backwards rate
+# α and β are the forward and backwards rate, respectively
 def alpha_n(V):
     return 0.01 * milli * (10 * milli - V)/ (np.exp((10 * milli - V) / (10 * milli)) - 1)
 def beta_n(V):
@@ -38,7 +31,21 @@ def m_inf(V):
 def h_inf(V):
     return alpha_h(V) / (alpha_h(V) + beta_h(V))
 
-def derivatives():
-    dn/dt = (alpha_n(V) * (1 - n)) - (beta_n(V) * n)
-    dm/dt = (alpha_m(V) * (1 - m)) - (beta_m(V) * m)
-    dh/dt = (alpha_h(V) * (1 - h)) - (beta_h(V) * h)
+
+def derivatives(param):
+    der = np.zeros(4)
+
+    # V_m is the membrane potential
+    # g_k and g_na are the potassium and sodium conductances per unit area
+    # g_l and V_l are the leak conductance per unit area and leak reversal potential
+    I_na = g_na * -(V_m - V_na)
+    I_k = g_k * -(V_m - V_k)
+    I_l = g_l * -(V_m - V_l)
+
+
+    der[0] = I_na/C_m + I_k/C_m + I_l/C_m    # dv/dt
+    der[1] = (alpha_n(V) * (1 - n)) - (beta_n(V) * n)    # dn/dt
+    der[2] = (alpha_m(V) * (1 - m)) - (beta_m(V) * m)    # dm/dt
+    der[3] = (alpha_h(V) * (1 - h)) - (beta_h(V) * h)    # dh/dt
+
+    return der
