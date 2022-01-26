222daccdd5854017f0fd210636bea5482d9b864c
stefanosxarak <stefanosxara@gmail.com>
2022-01-26 15:51:46 +0000

    solved ode and added graph

--- HH model.py
+++ HH model.py
@@ -9,48 +9,41 @@ def default_pars():
     pars = {}
     
     ### typical neuron parameters ###
-    pars['g_na']  = -55.  # Average sodoum channel conductance per unit area
-    pars['g_k']   = -75.  # Average potassium channel conductance per unit area
-    pars['g_l']   = 10.   # Average leak channel conductance per unit area
+    ### ALL UNITS NEED TO BE IN S.I. ###
+    pars['g_na']  = 120.0*milli   # Average sodoum channel conductance per unit area
+    pars['g_k']   = 36.0*milli    # Average potassium channel conductance per unit area
+    pars['g_l']   = 0.3*milli     # Average leak channel conductance per unit area
 
-    pars['n']     = 10.   # steady state values
-    pars['m']     = -65.  # steady state values
-    pars['h']     = -75.  # steady state values
+    pars['C_m']   = 0.000001     # Membrane capacitance per unit area
 
-    pars['V']     = 2.    # V is the membrane potential
-
-    pars['C_m']   = 2.    # Membrane capacitance per unit area
-
-    pars['Vk']    = 2.    # Potassium potential
-    pars['Vna']   = 2.    # Sodium potential
-    pars['Vl']    = 2.    # Leak potential
+    pars['V']     = 0.0*milli     # V is the membrane potential
+    pars['Vk']    = -12.0*milli   # Potassium potential
+    pars['Vna']   = 115.0*milli   # Sodium potential
+    pars['Vl']    = 10.613*milli  # Leak potential
 
     ### simulation parameters ###
     pars['tmin'] = 0
-    pars['tmax'] = 0
-    pars['T']    = np.linspace(pars['tmin'], pars['tmax'], 0) # Total duration of simulation [ms]
-    pars['dt']   = 0.1  # Simulation time step [ms]
+    pars['tmax'] = 50
+    pars['T']    = np.linspace(pars['tmin'], pars['tmax'], 10000) # Total duration of simulation [ms]
     
-    
-    pars['range_t'] = np.arange(0, pars['T'], pars['dt']) # Vector of discretized time points [ms]
-        
+            
     return pars
 
 # α and β are the forward and backwards rate, respectively
 def alpha_n(V):
-    return 0.01 * milli * (10 * milli - V)/ (np.exp((10 * milli - V) / (10 * milli)) - 1)
+    return 0.01 * milli * (10 * milli - V)/ (np.exp((10 * milli - V) / (10 * milli)) - 1*milli)
 def beta_n(V):
     return 0.125 * milli * np.exp(- V / (80 * milli))
 
 def alpha_m(V):
-    return 0.1 * milli * (25 * milli - V)/ (np.exp((25 * milli - V) / (10 * milli)) - 1)
+    return 0.1 * milli * (25 * milli - V)/ (np.exp((25 * milli - V) / (10 * milli)) - 1*milli)
 def beta_m(V):
     return 4 * milli  * np.exp(- V / (18 * milli))
 
 def alpha_h(V):
     return 0.07 * milli * np.exp(- V / (20 * milli))
 def beta_h(V):
-    return 1 / (np.exp((30 * milli -V) / (10 * milli)) + 1)
+    return 1*milli / (np.exp((30 * milli -V) / (10 * milli)) + 1*milli)
 
 def n_inf(V):
     return alpha_n(V) / (alpha_n(V) + beta_n(V))
@@ -62,19 +55,23 @@ def h_inf(V):
     return alpha_h(V) / (alpha_h(V) + beta_h(V))
 
 
-def derivatives(pars):
+def derivatives(y,t0):
     der = np.zeros(4)
+    pars = default_pars()
     C_m = pars['C_m']
-    V = pars['V']
+    
+    V = y[0]
+    n = y[1]
+    m = y[2]
+    h = y[3]
 
-    n = pars['n']
-    m = pars['m']
-    h = pars['h']
+    GNa = (pars['g_na'] / C_m) * np.power(m, 3.0) * h
+    GK = (pars['g_k'] / C_m) * np.power(n, 4.0)
+    GL = pars['g_l'] / C_m
 
-
-    I_na = pars['g_na'] * (pars['Vna'] - V)
-    I_k = pars['g_k'] * (pars['Vk'] - V)
-    I_l = pars['g_l'] * (pars['Vl'] - V)
+    I_na = GNa * (pars['Vna'] - V)
+    I_k = GK * (pars['Vk'] - V)
+    I_l = GL * (pars['Vl'] - V)
 
 
     der[0] = I_na/C_m + I_k/C_m + I_l/C_m    # dv/dt
@@ -86,6 +83,17 @@ def derivatives(pars):
 
 pars = default_pars()
 
-y = np.array([pars['V'], n_inf(), m_inf(), h_inf()])
+V = pars['V']
 
-sol = odeint(derivatives(pars), y, t)    # Solve ODE
+Y = np.array([V, n_inf(V), m_inf(V), h_inf(V)])
+
+sol = odeint(derivatives, Y, pars['T'])    # Solve ODE
+
+# Neuron potential
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(pars['T'], sol[:, 0])
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('V (V)')
+ax.set_title('Neuron potential with two spikes')
+plt.grid()
+plt.show()
