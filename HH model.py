59ae4e2808ed80736a195e05ec60de195bc2cf18
stefanosxarak <stefanosxara@gmail.com>
2022-01-27 20:39:58 +0000

    removed conversion from α and β

--- HH model.py
+++ HH model.py
@@ -10,50 +10,49 @@ class HodgkinHuxley():
     def default_pars(self):
         ### ALL UNITS NEED TO BE IN S.I. ###
         # Paremeters are passed from the command line when the program is executed
-        print("Please Enter all the values in S.I. units")
-        self.g_na = float(input("Enter the value of gNa: "))                        # Average sodoum channel conductance per unit area
-        self.g_k  = float(input("Enter the value of gK: "))                         # Average potassium channel conductance per unit area
-        self.g_l  = float(input("Enter the value of gl: "))                         # Average leak channel conductance per unit area
-        self.C_m  = float(input("Enter the value of membrane capacitance C_m: "))   # Membrane capacitance per unit area
-        self.V    = float(input("Enter the value of the membrane potential V: "))   # V is the membrane potential
-        self.Vna  = float(input("Enter the value of VNa: "))                        # Potassium potential
-        self.Vk   = float(input("Enter the value of VK: "))                         # Sodium potential
-        self.Vl   = float(input("Enter the value of Vl: "))                         # Leak potential
-        self.tmin = float(input("Enter the start time : "))
-        self.tmax = float(input("Enter the end time: "))
+       
+        # print("Please Enter all the values in S.I. units")
+        # self.g_na = float(input("Enter the value of gNa: "))                        # Average sodoum channel conductance per unit area
+        # self.g_k  = float(input("Enter the value of gK: "))                         # Average potassium channel conductance per unit area
+        # self.g_l  = float(input("Enter the value of gl: "))                         # Average leak channel conductance per unit area
+        # self.C_m  = float(input("Enter the value of membrane capacitance C_m: "))   # Membrane capacitance per unit area
+        # self.V    = float(input("Enter the value of the membrane potential V: "))   # V is the membrane potential
+        # self.Vna  = float(input("Enter the value of VNa: "))                        # Potassium potential
+        # self.Vk   = float(input("Enter the value of VK: "))                         # Sodium potential
+        # self.Vl   = float(input("Enter the value of Vl: "))                         # Leak potential
+        # self.tmin = float(input("Enter the start time : "))
+        # self.tmax = float(input("Enter the end time: "))
         
 
-        # pars={}
-        # pars['g_na']  = 120.0*milli  
-        # pars['g_k']   = 36.0*milli    
-        # pars['g_l']   = 0.3*milli    
-        # pars['C_m']   = 0.000001     
-        # pars['V']     = 0.0*milli     
-        # pars['Vk']    = -12.0*milli   
-        # pars['Vna']   = 115.0*milli   
-        # pars['Vl']    = 10.613*milli  
+        self.g_na = 40*milli/0.0001                      
+        self.g_k  = 35*milli/0.0001                         
+        self.g_l  = 0.3*milli/0.0001                          
+        self.C_m  = 1*milli*milli/0.0001 
+        self.V    = -65*milli
+        self.Vna  = 55*milli                        
+        self.Vk   = -77*milli                         
+        self.Vl   = -65*milli                         
+        self.tmin = 0*milli
+        self.tmax = 15*milli                                               
 
-        ### simulation parameters ###
-        # pars['tmin'] = 0
-        # pars['tmax'] = 30*milli
 
-        self.T    = np.linspace(self.tmin, self.tmax, 100) # Total duration of simulation [ms]
+        self.T  = np.linspace(self.tmin, self.tmax, 100) # Total duration of simulation [ms]
 
     # α and β are the forward and backwards rate, respectively
     def alpha_n(self,V):
-        return 0.01 * milli * (10 * milli - V)/ (np.exp((10 * milli - V) / (10 * milli)) - 1*milli)
+        return 0.01 * (10 - V)/ (np.exp((10 - V) / 10) - 1)
     def beta_n(self,V):
-        return 0.125 * milli * np.exp(- V / (80 * milli))
+        return 0.125 * np.exp(- V / 80)
 
     def alpha_m(self,V):
-        return 0.1 * milli * (25 * milli - V)/ (np.exp((25 * milli - V) / (10 * milli)) - 1*milli)
+        return 0.1  * (25 - V)/ (np.exp((25 - V) / 10) - 1)
     def beta_m(self,V):
-        return 4 * milli  * np.exp(- V / (18 * milli))
+        return 4 * np.exp(- V / 18)
 
     def alpha_h(self,V):
-        return 0.07 * milli * np.exp(- V / (20 * milli))
+        return 0.07 * np.exp(- V / 20)
     def beta_h(self,V):
-        return 1*milli / (np.exp((30 * milli -V) / (10 * milli)) + 1*milli)
+        return 1 / (np.exp((30 -V) / 10) + 1)
 
     def n_inf(self,V):
         return self.alpha_n(V) / (self.alpha_n(V) + self.beta_n(V))
@@ -67,10 +66,10 @@ class HodgkinHuxley():
     def Id(self,t):
         # time varying current
         # no specific criteria for the time selected
-        if 0.0 < t < 2:
+        if 0.0 < t < 1:
             self.I_inj = 150.0
             return self.I_inj
-        elif 10.0 < t < 13.0:
+        elif 10.0 < t < 11.0:
             self.I_inj = 50.0
             return self.I_inj
         return 0.0
