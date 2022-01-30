3d8d14cb3926ef3b982abee7f757d77e03cba4c3
stefanosxarak <stefanosxara@gmail.com>
2022-01-30 17:30:44 +0000

    type corrections and conversion units

--- HH model.py
+++ HH model.py
@@ -10,24 +10,34 @@ class HodgkinHuxley():
         ### ALL UNITS NEED TO BE IN S.I. ###
         # Paremeters are passed from the command line when the program is executed
        
-        # print("Please Enter all the values in milli units")
+        print("Please Enter all the values in S.I units")
         self.g_na = float(input("Enter the value of gNa: "))                        # Average sodoum channel conductance per unit area
         self.g_k  = float(input("Enter the value of gK: "))                         # Average potassium channel conductance per unit area
         self.g_l  = float(input("Enter the value of gl: "))                         # Average leak channel conductance per unit area
-        self.C_m  = float(input("Enter the value of membrane capacitance C_m: "))   # Membrane capacitance per unit area
+        self.C_m  = float(input("Enter the value of membrane capacitance in μ units: "))   # Membrane capacitance per unit area
         self.V    = float(input("Enter the value of the membrane potential V: "))   # V is the membrane potential
         self.Vna  = float(input("Enter the value of VNa: "))                        # Potassium potential
         self.Vk   = float(input("Enter the value of VK: "))                         # Sodium potential
         self.Vl   = float(input("Enter the value of Vl: "))                         # Leak potential
         self.tmin = float(input("Enter the start time : "))
         self.tmax = float(input("Enter the end time: "))
-        
+                
+        # self.g_na = 120               
+        # self.g_k  = 36                         
+        # self.g_l  = 0.3                   
+        # self.C_m  = 1
+        # self.V    = -25
+        # self.Vna  = 115                        
+        # self.Vk   = -12                        
+        # self.Vl   = 10.613                         
+        # self.tmin = 0
+        # self.tmax = 20  
 
-        self.T  = np.linspace(self.tmin*milli, self.tmax*milli, 100) # Total duration of simulation [ms]
+        self.T  = np.linspace(self.tmin, self.tmax, 100) # Total duration of simulation [ms]
 
 
     # α and β are the forward and backwards rate, respectively
-    # These are the original HH equations for α,β constants vary depending on the experimental data
+    # These are the original HH equations for α,β where the constants vary in order to fit adequately the data
     # α(v) = (A+B*V)/C + H*exp((V+D) / F) where A,B,C,D,F,H are to be fit to the data
     # NOTE: for cases where 0/0 might occur then L'Hospital's rules must apply
     def alpha_n(self,V):
@@ -72,11 +82,11 @@ class HodgkinHuxley():
     def Id(self,t):
         # time varying current
         # no specific criteria for the time selected
-        if 0.0 < t < 1:
-            self.I_inj = 150.0
+        if 2.0 < t < 3.0:
+            self.I_inj = 100.0
             return self.I_inj
         elif 10.0 < t < 11.0:
-            self.I_inj = 50.0
+            self.I_inj = 40.0
             return self.I_inj
         return 0.0
 
@@ -90,16 +100,16 @@ class HodgkinHuxley():
         self.m = y[2]
         self.h = y[3]
 
-        GNa = (self.g_na / C_m) * np.power(self.m, 3.0) * self.h
-        GK = (self.g_k / C_m) * np.power(self.n, 4.0)
-        GL = self.g_l / C_m
+        GNa = self.g_na * np.power(self.m, 3.0) * self.h
+        GK = self.g_k * np.power(self.n, 4.0)
+        GL = self.g_l
 
         self.I_na = GNa * (V - self.Vna )
         self.I_k = GK * (V - self.Vk )
         self.I_l = GL * (V - self.Vl )
 
 
-        der[0] = (self.Id(t0) / C_m) - self.I_na + self.I_k + self.I_l   # dv/dt
+        der[0] = (self.Id(t0) - self.I_na - self.I_k - self.I_l) / C_m   # dv/dt
         der[1] = (self.alpha_n(V) * (1 - self.n)) - (self.beta_n(V) * self.n)    # dn/dt
         der[2] = (self.alpha_m(V) * (1 - self.m)) - (self.beta_m(V) * self.m)    # dm/dt
         der[3] = (self.alpha_h(V) * (1 - self.h)) - (self.beta_h(V) * self.h)    # dh/dt
@@ -111,25 +121,15 @@ class HodgkinHuxley():
 
         V = self.V
         t = self.T
-        Y = np.array([V, self.n_inf(V), self.m_inf(V), self.h_inf(V)])
+        Y = np.array([V, self.n_inf(V), self.m_inf(V), self.h_inf(V)], dtype= 'float64')
 
         sol = odeint(self.derivatives, Y, t)    # Solve ODE
 
-        # Neuron potential
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.plot(t, sol[:, 0]*milli)
+        fig, ax = plt.subplots(figsize=(9, 6))
+        ax.plot(t, sol[:, 0])
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Membrane potential (V)')
         ax.set_title('Neuron potential')
-        plt.grid()
-        plt.show()
-
-        Idv = [Id(l) for l in t]
-        fig, ax = plt.subplots(figsize=(12, 7))
-        ax.plot(t, Idv)
-        ax.set_xlabel('Time (ms)')
-        ax.set_ylabel(r'Current density (uA/$cm^2$)')
-        ax.set_title('Stimulus (Current density)')
         plt.grid()
         plt.show()
 
