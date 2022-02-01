536818712a0671bdfd4e7d6101082ff0422cac45
stefanosxarak <stefanosxara@gmail.com>
2022-02-01 17:51:51 +0000

    removed limit added conversion at the end

--- HH model.py
+++ HH model.py
@@ -2,7 +2,6 @@ from Units import *
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from sympy import limit,symbols,E
 
 # Nelson, M.E. (2004) Electrophysiological Models In: Databasing the Brain: From Data to Knowledge. (S. Koslow and S. Subramaniam, eds.) Wiley, New York.
 
@@ -12,11 +11,11 @@ class HodgkinHuxley():
         ### ALL UNITS NEED TO BE IN S.I. ###
         # Paremeters are passed from the command line when the program is executed
        
-        # print("Please Enter all the values in S.I units")
+        # print("Please Enter all the values in milli units (graphs will automatically convert to S.I. units)")
         # self.g_na = float(input("Enter the value of gNa: "))                        # Average sodoum channel conductance per unit area
         # self.g_k  = float(input("Enter the value of gK: "))                         # Average potassium channel conductance per unit area
         # self.g_l  = float(input("Enter the value of gl: "))                         # Average leak channel conductance per unit area
-        # self.C_m  = float(input("Enter the value of membrane capacitance in μ units: "))   # Membrane capacitance per unit area
+        # self.C_m  = float(input("Enter the value of membrane capacitance in μ (micro) units: "))   # Membrane capacitance per unit area
         # self.v   = float(input("Enter the value of the membrane potential v: "))   # vis the membrane potential
         # self.vna  = float(input("Enter the value of vNa: "))                        # Potassium potential
         # self.vk   = float(input("Enter the value of vK: "))                         # Sodium potential
@@ -25,18 +24,18 @@ class HodgkinHuxley():
         # self.tmax = float(input("Enter the end time: "))
                 
         self.g_na = 120.               
-        self.g_k  = 36.0                         
+        self.g_k  = 36.                       
         self.g_l  = 0.3                   
-        self.c_m  = 1.0
+        self.c_m  = 1.
         self.v   = -65.
         self.vna  = 115.                        
         self.vk   = -12.                        
         self.vl   = 10.613                         
         self.tmin = 0.
-        self.tmax = 35.0  
+        self.tmax = 35.  
         self.i_inj = 50.
 
-        self.t  = np.linspace(self.tmin, self.tmax, 100) # Total duration of simulation [ms]
+        self.t  = np.linspace(self.tmin, self.tmax, 1000) # Total duration of simulation [ms]
 
 
     # α and β are the forward and backwards rate, respectively
@@ -45,26 +44,24 @@ class HodgkinHuxley():
     
     # NOTE: for cases where 0/0 might occur then L'Hospital's rules must apply
     def alpha_n(self,v):
-        x = symbols('x')
-        A = 0.01 * (10 - v)
-        B = (np.exp((10 - v) / 10) - 1)
+        nom = 0.01 * (10 - v)
+        denom = (np.exp((10 - v) / 10) - 1)
 
-        if A == 0 and B == 0 :
-            return limit(0.01 * (10 - x) /(E**((10 - x) / 10) - 1), x,10)
+        if nom == 0 and denom == 0 :
+            return 0.1
         else:
-            return A / B
+            return nom / denom
     def beta_n(self,v):
         return 0.125 * np.exp(- v/ 80)
 
     def alpha_m(self,v):
-        x = symbols('x')
-        A = 0.1  * (25 - v)
-        B = (np.exp((25 - v) / 10) - 1)
+        nom = 0.1  * (25 - v)
+        denom = (np.exp((25 - v) / 10) - 1)
 
-        if A == 0 and B == 0 :
-            return limit(0.1  * (25 - x) / (E**((25 - x) / 10) - 1), x,10)
+        if nom == 0 and denom == 0 :
+            return 1.5/(-1 + np.exp(3/2))
         else:
-            return A / B
+            return nom / denom
         
     def beta_m(self,v):
         return 4 * np.exp(- v/ 18)
@@ -94,6 +91,11 @@ class HodgkinHuxley():
     #         return self.i_inj
     #     return 0.0
 
+    # def frequency(self):
+    #     freq = []
+    #     for i in range(len(self.i_inj)):
+    #         rec_spikes = 
+    #         freq.append(rec_spikes/self.t)
 
     def derivatives(self,y,t):
         der = np.zeros(4)
@@ -129,8 +131,9 @@ class HodgkinHuxley():
 
         sol = odeint(self.derivatives, y, t)    # Solve ODE
 
+
         ax = plt.subplot()
-        ax.plot(t, sol[:, 0])
+        ax.plot(t*milli, sol[:, 0]*milli)
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Membrane potential (v)')
         ax.set_title('Neuron potential')
@@ -138,9 +141,9 @@ class HodgkinHuxley():
         plt.show()
 
         ax = plt.subplot()
-        ax.plot(t, sol[:,1], 'r', label='n')
-        ax.plot(t, sol[:,2], 'g', label='m')
-        ax.plot(t, sol[:,3], 'b', label='h')
+        ax.plot(t*milli, sol[:,1], 'r', label='n')
+        ax.plot(t*milli, sol[:,2], 'g', label='m')
+        ax.plot(t*milli, sol[:,3], 'b', label='h')
         ax.set_ylabel('Gating value')
         ax.set_xlabel('Time (s)')
         ax.set_title('Potassium and Sodium channels')
