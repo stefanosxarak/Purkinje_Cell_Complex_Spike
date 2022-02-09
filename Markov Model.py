e57b2142b270fea885159218a44d0b68ac3e56ee
stefanosxarak <stefanosxara@gmail.com>
2022-02-09 12:45:47 +0000

    added sequential inactiveted states derivatives

--- Markov Model.py
+++ Markov Model.py
@@ -12,8 +12,8 @@ class Markov():
     def ksi(self,v):
         return 0.03 * np.exp(-v/25*mv)
 
-    def derivatives(self):
-        v   = 0.
+    def derivatives(self,c,I,O,B):
+        v = 0.
         γ = 150         # m*s**(-1)
         δ = 40          # m*s**(-1)
         ε = 1.75        # m*s**(-1)
@@ -29,25 +29,30 @@ class Markov():
 
         der = np.zeros(13)
         
-        der[0] = U * I[0] + β*c[1] - c[0]*4*α - D*c[0]   # dC1/dt
-        der[1] = U/a * I[1] + 2*β*c[2] - c[1]*3*α - D*a*c[1]   # dC2/dt
-        der[2] = (U/(a**2)) * I[2] + 3*β*c[3] - c[2]*2*α - D*a**2*c[2]   # dC3/dt
-        der[3] = (U/(a**3)) * I[3] + 4*β*c[4] - c[3]*α - D*a**3*c[3]   # dC4/dt
-        der[4] = (U/(a**4)) * I[4] + δ*O - c[4]*γ - D*a**4*c[4]   # dC5/dt
+        der[0] = U * I[0] + β*c[1] - c[0]*4*α - D*c[0]                  # dC1/dt
+        der[1] = U/a * I[1] + 2*β*c[2] - c[1]*3*α - D*a*c[1]            # dC2/dt
+        der[2] = (U/(a**2)) * I[2] + 3*β*c[3] - c[2]*2*α - D*a**2*c[2]  # dC3/dt
+        der[3] = (U/(a**3)) * I[3] + 4*β*c[4] - c[3]*α - D*a**3*c[3]    # dC4/dt
+        der[4] = (U/(a**4)) * I[4] + δ*O - c[4]*γ - D*a**4*c[4]         # dC5/dt
 
-        # der[5] =
-        # der[6] =
-        # der[7] =
-        # der[8] =
-        # der[9] =
-        # der[10] =
+        der[5]  = D * c[0] + 4*β/a *I[1] - I[0] *U - I[0] *a*α                #dI1/dt
+        der[6]  = a* D * c[1] + 3*β/a *I[2] - I[1] *U/a - I[1] *a*α*2         #dI2/dt
+        der[7]  = a**2* D * c[2] + 2*β/a *I[3] - I[2] *U/a**2 - I[2] *a*α*3   #dI3/dt
+        der[8]  = a**3* D * c[3] + β/a *I[4] - I[3] *U/a**3 - I[3] *a*α*4     #dI4/dt
+        der[9]  = a**4*D*c[4] + δ *I[5] - I[4]*U/a**4 - I[4]*γ                #dI5/dt
+        der[10] = N*O - I[5]*F                                                #dI6/dt
 
         der[11] = ξ*B + F*I[5] - O*N - O*ε  # dO/dt
         der[12] = O * ε - B*ξ   # dB/dt
 
         print(der[0]+der[1]+der[5])
+        print(c[0]+c[1]+I[0])
+
+        return der
+
     def Main(self):
-        markov = odeint(self.derivatives, y, t)    # Solve ODE
+        self.derivatives(c,I,O,B)
+        # markov = odeint(self.derivatives, y, t)    # Solve ODE
 
 if __name__ == '__main__':
     runner = Markov()
