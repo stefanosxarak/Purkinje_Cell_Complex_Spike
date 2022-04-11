fixed normalisation,parameters might be wrong

Full_Model.py
@@ -10,7 +10,7 @@ import time
 # K = Potassium
 
 start_time = time.time()    # Monitor performance
-tmax = 10                   # Duration of the simulation
+tmax = 35                   # Duration of the simulation
 i_na_trace = []             # Trace for graph purposes
 i_k_trace = []
 i_leak_trace = []
@@ -42,7 +42,6 @@ class Somatic_Voltage:
         i_na_trace.append(i_na)
         i_k_trace.append(i_k)
         i_leak_trace.append(i_l)
-
         deriv = (self.i_inj - i_na - i_k - i_l) / self.c_m                          # dV/dt
 
         return deriv
@@ -82,7 +81,7 @@ hh = HodgkinHuxley()
 
 somatic_voltage=Somatic_Voltage(g_na=120., g_k=36., g_l=0.3, c_m=1., v_init=-70., vna=115., vk=-12., vl=10.613, i_inj=10.)
 # somatic_voltage=Somatic_Voltage(105.,15.,2.,1.,-70.,45.,-88.,-88,10.)              # Parameter values from Parameters2.py
-# somatic_voltage=Somatic_Voltage(47.2,200.,2.,1.,10.,45.,-88.,-88.,10.)             # Parameter values from research paper
+# somatic_voltage=Somatic_Voltage(47.2,200.,2.,1.,-50,45.,-88.,-88.,10.)             # Parameter values from research paper
 # somatic_voltage=Somatic_Voltage(36.,120.,0.3,1.,0.,-115.,12.,-10.613,10.)          # Parameter values from 2013 Action potential modelling
 
 v_initial = somatic_voltage.v_init
@@ -95,37 +94,46 @@ bigv=bigt=bigo=bigb=bigi6=bigc5= np.array([])
 
 i=0
 status = 0
-step = 0.025 
+step = 0.0025
+j = 10 
+enter = 1
+norm_c=1
+print_c = 1
+print_n = 100
 while i< tmax and status==0 :
+    # print(i)
+    result = solve_ivp(f, t_span=(i,i+step), y0=y,  method='BDF')
+    # print(np.shape(result.y))                                      # (15,len(t_eval)) 15 derivatives and the length of t_eval if one exists
+    if norm_c == 1:
+        # print(enter)
+        enter+=1 
+        y_norm = result.y[:,-1]
+        y = normalize(y_norm)                                        # Normalise only the markov derivatives at every step
+        norm_c= j
+    if print_c ==1:
+        print_c=print_n
+        bigv  = np.concatenate((bigv,result.y[0]))                       # result.y[0] = result.y[0,:]
+        bigt  = np.concatenate((bigt,result.t))   
+        bigc5 = np.concatenate((bigc5,result.y[6]))     
+        bigi6 = np.concatenate((bigi6,result.y[12]))
+        bigo  = np.concatenate((bigo,result.y[13]))
+        bigb  = np.concatenate((bigb,result.y[14]))
 
-    result = solve_ivp(f, t_span=(i,i+step), y0=y, t_eval=(np.linspace(i, i+step, 100)), method='BDF') 
-    y_norm = result.y[:,-1]
-    y = normalize(y_norm)                                   # Normalise only the markov derivatives at every step
+    status = result.status                                       # -1: Integration step failed.
+    i+=step                                                      #  0: The solver successfully reached the end of t_span.
+    norm_c  -= 1
+    print_c -= 1  
 
-    # print(np.shape(result.y))                               # (15,len(t_eval)) 15 derivatives and the length of t_eval if one exists
-
-    bigv = np.concatenate((bigv,result.y[0]))               # result.y[0] = result.y[0,:]
-    bigt = np.concatenate((bigt,result.t))   
-
-    bigc5 = np.concatenate((bigc5,result.y[6]))    
-    bigi6 = np.concatenate((bigi6,result.y[12]))
-    bigo = np.concatenate((bigo,result.y[13]))
-    bigb = np.concatenate((bigb,result.y[14]))
-
-
-    status = result.status                                     # -1: Integration step failed.
-    i+=step                                                    #  0: The solver successfully reached the end of t_span.
-
-# ax = plt.subplot()
-# ax.plot(bigt, bigo,c='r',label='o')
-# ax.plot(bigt, bigb,c='b',label='b')
-# ax.plot(bigt, bigi6,c='orange',label='i6')
-# ax.plot(bigt, bigc5,c='g',label='c5')
-# ax.set_xlabel('Time (ms)')
-# ax.set_ylabel('Fraction')
-# plt.legend()
-# plt.grid()
-# plt.show()
+ax = plt.subplot()
+ax.plot(bigt, bigo,c='r',label='o')
+ax.plot(bigt, bigb,c='b',label='b')
+ax.plot(bigt, bigi6,c='orange',label='i6')
+ax.plot(bigt, bigc5,c='g',label='c5')
+ax.set_xlabel('Time (ms)')
+ax.set_ylabel('Fraction')
+plt.legend()
+plt.grid()
+plt.show()
 
 ax = plt.subplot()
 ax.plot(bigt, bigv)

Markov_Model.py
@@ -38,12 +38,12 @@ class Markov:                               # Current through the sodium channel
         c2 = y[2]
         c3 = y[3]
         c4 = y[4]
-        I0 = y[5]
-        I1 = y[6]
-        I2 = y[7]
-        I3 = y[8]
-        I4 = y[9]
-        I5 = y[10]
+        i0 = y[5]
+        i1 = y[6]
+        i2 = y[7]
+        i3 = y[8]
+        i4 = y[9]
+        i5 = y[10]
         o  = y[11]
         b  = y[12] 
 
@@ -60,18 +60,18 @@ class Markov:                               # Current through the sodium channel
 
         der = np.zeros(13)
         
-        der[0] = u * I0 + β*c1 - c0*4*α - d*c0                                       # dC1/dt
-        der[1] = u/a*I1 + 2*β*c2+4*α*c0-(3*α+ β + d*a)*c1                            # dC2/dt
-        der[2] = (u/(a**2)) *I2 +3*β *c3 + 3*α *c1-(2*α +2*β + d* a**2)*c2           # dC3/dt
-        der[3] = (u/(a**3)) *I3+ 4*β *c4 +2*α *c2-(α + 3*β + d* a**3)*c3             # dC4/dt
-        der[4] = (u/(a**4)) *I4+ δ*o+ α*c3 -(γ +4*β + d*a**4)*c4                     # dC5/dt
-        der[5]  = d * c0 + 4*β/a *I1 - I0 *u - I0 *a*α                               # dI1/dt
-        der[6]  = d*a *c1 + 3*bav*I2 + aav*I0      - (u/a + 2*aav + 4*bav) *I1       # dI2/dt
-        der[7]  = d*a**2 *c2 + 2*bav*I3 + 2*aav*I1 - (u/a**2 + 3*aav + 3*bav) *I2    # dI3/dt
-        der[8]  = d*a**3 *c3 + bav*I4 + 3*aav*I2   - (u/a**3 + 4*aav + 2*bav) *I3    # dI4/dt
-        der[9]  = d*a**4 *c4 + δ*I5 + 4*aav*I3     - (u/a**4 + γ + bav) *I4          # dI5/dt
-        der[10] = n*o + γ*I4 - (f + δ) *I5                                           # dI6/dt
-        der[11] = γ* c4+ ξ*b+ f*I5 - (δ + n + ε)*o                                   # do/dt
+        der[0] = u * i0 + β*c1 - c0*4*α - d*c0                                       # dC1/dt
+        der[1] = u/a*i1 + 2*β*c2+4*α*c0-(3*α+ β + d*a)*c1                            # dC2/dt
+        der[2] = (u/(a**2)) *i2 +3*β *c3 + 3*α *c1-(2*α +2*β + d* a**2)*c2           # dC3/dt
+        der[3] = (u/(a**3)) *i3+ 4*β *c4 +2*α *c2-(α + 3*β + d* a**3)*c3             # dC4/dt
+        der[4] = (u/(a**4)) *i4+ δ*o+ α*c3 -(γ +4*β + d*a**4)*c4                     # dC5/dt
+        der[5]  = d * c0 + 4*β/a *i1 - i0 *u - i0 *a*α                               # di1/dt
+        der[6]  = d*a *c1 + 3*bav*i2 + aav*i0      - (u/a + 2*aav + 4*bav) *i1       # di2/dt
+        der[7]  = d*a**2 *c2 + 2*bav*i3 + 2*aav*i1 - (u/a**2 + 3*aav + 3*bav) *i2    # di3/dt
+        der[8]  = d*a**3 *c3 + bav*i4 + 3*aav*i2   - (u/a**3 + 4*aav + 2*bav) *i3    # di4/dt
+        der[9]  = d*a**4 *c4 + δ*i5 + 4*aav*i3     - (u/a**4 + γ + bav) *i4          # di5/dt
+        der[10] = n*o + γ*i4 - (f + δ) *i5                                           # dI6/dt
+        der[11] = γ* c4+ ξ*b+ f*i5 - (δ + n + ε)*o                                   # do/dt
         der[12] = o * ε - b*ξ                                                        # db/dt
 
         return der
@@ -84,7 +84,7 @@ class Markov:                               # Current through the sodium channel
         # self.bigt = np.array([])
         # self.bigo = np.array([])
         # self.bigb = np.array([])
-        # y = np.array([self.c0,self.c1,self.c2,self.c3,self.c4,self.I0,self.I1,self.I2,self.I3,self.I4,self.I5,self.o,self.b])
+        # y = np.array([self.c0,self.c1,self.c2,self.c3,self.c4,self.i0,self.i1,self.i2,self.i3,self.i4,self.i5,self.o,self.b])
 
         # j=0 # time step being self.tmax/len(v)
         # for i in range(0,len(v)):